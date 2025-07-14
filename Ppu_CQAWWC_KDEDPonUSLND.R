8bf917452967b3958ce258e72abeb74b7523ae61
Chandler Xiande Yang <45187869+ChandlerXiandeYang@users.noreply.github.com>
2025-07-14 14:53:11 -0400

    Add files via upload

---
+++ Ppu_CQAWWC_KDEDPonUSLND.R
@@ -0,0 +1,24 @@
+Ppu_CQAWWC_KDEDPonUSLND <- function(data1, Residue1, USL1,
+                                    data2 = NULL, Residue2 = NULL, USL2 = NULL,
+                                    data3 = NULL, Residue3 = NULL, USL3 = NULL,
+                                    BW = "Silver1.06") {
+  ppu_list <- list()
+  
+  # Dataset 1
+  ppu1 <- Ppu_KDEDPonUSLND(data1, {{ Residue1 }}, {{ USL1 }}, BW)$Ppu
+  ppu_list <- c(ppu_list, ppu1)
+  
+  # Dataset 2
+  if (!is.null(data2) && !rlang::quo_is_null(rlang::enquo(Residue2)) && !rlang::quo_is_null(rlang::enquo(USL2))) {
+    ppu2 <- Ppu_KDEDPonUSLND(data2, {{ Residue2 }}, {{ USL2 }}, BW)$Ppu
+    ppu_list <- c(ppu_list, ppu2)
+  }
+  
+  # Dataset 3
+  if (!is.null(data3) && !rlang::quo_is_null(rlang::enquo(Residue3)) && !rlang::quo_is_null(rlang::enquo(USL3))) {
+    ppu3 <- Ppu_KDEDPonUSLND(data3, {{ Residue3 }}, {{ USL3 }}, BW)$Ppu
+    ppu_list <- c(ppu_list, ppu3)
+  }
+  
+  return(min(unlist(ppu_list), na.rm = TRUE))
+}
