f075ee5e29a29809cc66708e69ba1ec3bf458fc8
Chandler Xiande Yang <45187869+ChandlerXiandeYang@users.noreply.github.com>
2025-07-14 16:09:58 -0400

    Update Ppu_CQAWWC_KDEDPonUSLND.R

--- Ppu_CQAWWC_KDEDPonUSLND.R
+++ Ppu_CQAWWC_KDEDPonUSLND.R
@@ -2,23 +2,29 @@ Ppu_CQAWWC_KDEDPonUSLND <- function(data1, Residue1, USL1,
                                     data2 = NULL, Residue2 = NULL, USL2 = NULL,
                                     data3 = NULL, Residue3 = NULL, USL3 = NULL,
                                     BW = "Silver1.06") {
-  ppu_list <- list()
+  ppu_named <- c()
   
   # Dataset 1
   ppu1 <- Ppu_KDEDPonUSLND(data1, {{ Residue1 }}, {{ USL1 }}, BW)$Ppu
-  ppu_list <- c(ppu_list, ppu1)
+  ppu_named <- c(ppu_named, Ppu_Data1 = ppu1)
   
   # Dataset 2
   if (!is.null(data2) && !rlang::quo_is_null(rlang::enquo(Residue2)) && !rlang::quo_is_null(rlang::enquo(USL2))) {
     ppu2 <- Ppu_KDEDPonUSLND(data2, {{ Residue2 }}, {{ USL2 }}, BW)$Ppu
-    ppu_list <- c(ppu_list, ppu2)
+    ppu_named <- c(ppu_named, Ppu_Data2 = ppu2)
   }
   
   # Dataset 3
   if (!is.null(data3) && !rlang::quo_is_null(rlang::enquo(Residue3)) && !rlang::quo_is_null(rlang::enquo(USL3))) {
     ppu3 <- Ppu_KDEDPonUSLND(data3, {{ Residue3 }}, {{ USL3 }}, BW)$Ppu
-    ppu_list <- c(ppu_list, ppu3)
+    ppu_named <- c(ppu_named, Ppu_Data3 = ppu3)
   }
   
-  return(min(unlist(ppu_list), na.rm = TRUE))
+  # Calculate minimal Ppu from all datasets
+  min_ppu <- min(ppu_named, na.rm = TRUE)
+  
+  # Combine minimal Ppu with all individual values
+  output <- c(Ppu_Min = min_ppu, ppu_named)
+  
+  return(output)
 }
