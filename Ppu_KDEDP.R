1cdd73e5f8bb5b87e07172aaa85de23c66a41771
Xiande (Chandler) Yang <45187869+ChandlerXiandeYang@users.noreply.github.com>
2025-07-25 16:23:30 -0400

    Update Ppu_KDEDP.R

--- Ppu_KDEDP.R
+++ Ppu_KDEDP.R
@@ -1,7 +1,7 @@
 Ppu_KDEDP <- function(data, Residue, USL, BW = "Silver1.06") {
   library(dplyr)
   library(rlang)
-  
+
   residue_quo <- enquo(Residue)
   usl_quo <- enquo(USL)
   data_clean <- data %>%
@@ -10,13 +10,13 @@ Ppu_KDEDP <- function(data, Residue, USL, BW = "Silver1.06") {
       Residue_Val = !!residue_quo,
       USL_Val = !!usl_quo
     )
-  
+
   if (nrow(data_clean) == 0) stop("No valid rows remaining after removing NAs in Residue or USL.")
   x <- data_clean$Residue_Val
   usl_val <- data_clean$USL_Val[1]  # Assume constant USL
   n <- length(x)
-  #s <- sqrt(mean((x - mean(x))^2))  
-  s <- sd(x)
+  #s <- sqrt(mean((x - mean(x))^2))  #uncorrected standard deviation. JMP claimed it uses this one
+  s <- sd(x) #corrected standard deviation
   if (is.character(BW)) {
     BW <- match.arg(BW, choices = c("Silver1.06", "Silver0.9", "Silver0.9IQR"))
     h <- switch(BW,
@@ -43,9 +43,7 @@ Ppu_KDEDP <- function(data, Residue, USL, BW = "Silver1.06") {
   inv_cdf <- approxfun(Fx, x_vals, rule = 2)
   P0.5 <- inv_cdf(0.5)
   P0.99865 <- inv_cdf(0.99865)
-  
   Ppu <- (usl_val - P0.5) / (P0.99865 - P0.5)
-  
   df_result <- data.frame(
     Ppu = round(Ppu, 3),
     P0.5 = round(P0.5, 3),
@@ -55,6 +53,5 @@ Ppu_KDEDP <- function(data, Residue, USL, BW = "Silver1.06") {
     Bandwidth_Method = bw_method,
     Bandwidth_Value = round(h, 5)
   )
-  
   return(df_result)
 }
