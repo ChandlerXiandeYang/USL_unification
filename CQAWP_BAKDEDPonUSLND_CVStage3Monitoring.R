cb0d5f208f49ab94407be6465a7207615aa594bd
Xiande (Chandler) Yang <45187869+ChandlerXiandeYang@users.noreply.github.com>
2025-07-25 16:21:23 -0400

    Update CQAWP_BAKDEDPonUSLND_CVStage3Monitoring.R

--- CQAWP_BAKDEDPonUSLND_CVStage3Monitoring.R
+++ CQAWP_BAKDEDPonUSLND_CVStage3Monitoring.R
@@ -11,13 +11,10 @@ CQAWP_BAKDEDPonUSLND_CVStage3Monitoring <- function(
     seed = NULL
 ) {
   library(dplyr)
-  
   if (!is.null(seed)) set.seed(seed)
-  
   clean_na_residue <- function(data, residue_col) {
     data %>% filter(!is.na({{ residue_col }}))
   }
-  
   split_by_fiscal_year <- function(data, fiscal_year_col, time_cut) {
     training <- data %>% 
       filter(!is.na({{ fiscal_year_col }})) %>%
@@ -29,7 +26,6 @@ CQAWP_BAKDEDPonUSLND_CVStage3Monitoring <- function(
     
     list(training = training, testing = testing)
   }
-  
   split1 <- split_by_fiscal_year(data1, {{ Fiscal_Year1 }}, Time_cut)
   TrainingData1 <- clean_na_residue(split1$training, {{ Residue1 }})
   TestingData1 <- clean_na_residue(split1$testing, {{ Residue1 }})
@@ -42,7 +38,6 @@ CQAWP_BAKDEDPonUSLND_CVStage3Monitoring <- function(
     TrainingData2 <- NULL
     TestingData2 <- NULL
   }
-  
   if (!is.null(data3)) {
     split3 <- split_by_fiscal_year(data3, {{ Fiscal_Year3 }}, Time_cut)
     TrainingData3 <- clean_na_residue(split3$training, {{ Residue3 }})
@@ -51,7 +46,6 @@ CQAWP_BAKDEDPonUSLND_CVStage3Monitoring <- function(
     TrainingData3 <- NULL
     TestingData3 <- NULL
   }
-  
   if (is.null(CIL) || CIL < 1) {
     ppu_ci_obj <- Ppu_CQAWP_BAKDEDPonUSLND(
       data1 = TrainingData1, Residue1 = {{ Residue1 }}, USL1 = {{ USL1 }},
@@ -62,7 +56,6 @@ CQAWP_BAKDEDPonUSLND_CVStage3Monitoring <- function(
       conf_level = conf_level,
       seed = seed
     )
-    
     if (ppu_ci_obj$CI_lower < 1) {
       message("Initial CI lower bound < 1, increasing bootstrap to ", max_boot)
       ppu_ci_obj <- Ppu_CQAWP_BAKDEDPonUSLND(
@@ -86,26 +79,21 @@ CQAWP_BAKDEDPonUSLND_CVStage3Monitoring <- function(
       data3 = TrainingData3, Residue3 = {{ Residue3 }}, USL3 = {{ USL3 }},
       BW = BW
     )$Ppu
-    
     ppu_ci_obj <- list(
       Ppu = training_ppu,
       CI_lower = CIL
     )
   }
-  
   CombinedData1 <- bind_rows(TrainingData1, TestingData1)
   CombinedData2 <- if (!is.null(TrainingData2)) bind_rows(TrainingData2, TestingData2) else NULL
   CombinedData3 <- if (!is.null(TrainingData3)) bind_rows(TrainingData3, TestingData3) else NULL
-  
   testing_ppu_result <- Ppu_CQAWP_KDEDPonUSLND(
     data1 = CombinedData1, Residue1 = {{ Residue1 }}, USL1 = {{ USL1 }},
     data2 = CombinedData2, Residue2 = {{ Residue2 }}, USL2 = {{ USL2 }},
     data3 = CombinedData3, Residue3 = {{ Residue3 }}, USL3 = {{ USL3 }},
     BW = BW
   )
-  
   Testing_Ppu <- testing_ppu_result$Ppu
-  
   decision <- if (Testing_Ppu >= CIL) {
     "Cleaning process is capable."
   } else if (Testing_Ppu >= 1 && Testing_Ppu < CIL) {
@@ -113,7 +101,6 @@ CQAWP_BAKDEDPonUSLND_CVStage3Monitoring <- function(
   } else {
     "Cleaning process is NOT capable."
   }
-  
   output <- data.frame(
     Ppu_training = ppu_ci_obj$Ppu,
     Ppu_threshold = CIL,
@@ -121,7 +108,5 @@ CQAWP_BAKDEDPonUSLND_CVStage3Monitoring <- function(
     Performance_conclusion = decision,
     stringsAsFactors = FALSE
   )
-  
   return(output)
 }
-
