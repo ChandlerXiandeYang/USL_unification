9433527df1718b502727a016670557876ce75dcd
Xiande (Chandler) Yang <45187869+ChandlerXiandeYang@users.noreply.github.com>
2025-07-25 16:20:08 -0400

    Update Ppu_CQAWP_BAKDEDPonUSLND.R

--- Ppu_CQAWP_BAKDEDPonUSLND.R
+++ Ppu_CQAWP_BAKDEDPonUSLND.R
@@ -22,22 +22,16 @@ Ppu_CQAWP_BAKDEDPonUSLND <- function(data1, Residue1, USL1,
   data1_clean <- clean_data(data1, {{Residue1}}, {{USL1}})
   data2_clean <- if (!is.null(data2)) clean_data(data2, {{Residue2}}, {{USL2}}) else NULL
   data3_clean <- if (!is.null(data3)) clean_data(data3, {{Residue3}}, {{USL3}}) else NULL
-  
-  # Capture quosures for Residue and USL to use in bootstrap safely
   Residue1_quo <- enquo(Residue1)
   USL1_quo <- enquo(USL1)
   Residue2_quo <- if (!missing(Residue2)) enquo(Residue2) else NULL
   USL2_quo <- if (!missing(USL2)) enquo(USL2) else NULL
   Residue3_quo <- if (!missing(Residue3)) enquo(Residue3) else NULL
   USL3_quo <- if (!missing(USL3)) enquo(USL3) else NULL
-  
-  # 2) Bootstrap iterations
   bootstrap_ppus <- replicate(n_boot, {
     boot_data1 <- data1_clean %>% sample_n(size = nrow(data1_clean), replace = TRUE)
     boot_data2 <- if (!is.null(data2_clean)) data2_clean %>% sample_n(size = nrow(data2_clean), replace = TRUE) else NULL
     boot_data3 <- if (!is.null(data3_clean)) data3_clean %>% sample_n(size = nrow(data3_clean), replace = TRUE) else NULL
-    
-    # 3) Calculate Ppu on bootstrap samples, safely
     tryCatch({
       Ppu_CQAWP_KDEDPonUSLND(
         boot_data1, !!Residue1_quo, !!USL1_quo,
